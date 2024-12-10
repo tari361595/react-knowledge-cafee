b62c25ea3f34ac40fa761aabf0b42a54857ca821
tari361595 <cmtariqulislam1994@gmail.com>
2024-12-10 15:42:59 +0900

    props prototype declare

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Header from './components/Header'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
   
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
       <Header></Header>
+      <div className='flex'>
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
